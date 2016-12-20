b0f45bf28ebf4db35c6ee67aa2d5334a48790470
cheezgi <zackh@firemail.cc>
2016-12-20 19:39:11 +0000

    add lua test, ncurses test

--- source/es.c
+++ source/es.c
@@ -1,8 +1,22 @@
-#include <stdio.h>
 
-int
-main(int argc, char **argv)
-{
-    puts("Hello, world!");
+#include <stdio.h>
+#include <ncurses.h>
+#include "lua.h"
+#include "lauxlib.h"
+
+int main(int argc, char **argv) {
+    // lua test
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+    luaL_dofile(L, "test.lua");
+
+    // ncurses test
+    initscr();
+    cbreak();
+    printw("Hello from ncurses");
+    getch();
+    endwin();
+
     return 0;
 }
+
