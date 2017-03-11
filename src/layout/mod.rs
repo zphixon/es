91a7608bf2645ab1ca33d28f3c498c266dddabad
cheezgi <zackh@firemail.cc>
2017-03-11 18:32:58 -0500

    ugh

--- src/layout/mod.rs
+++ src/layout/mod.rs
@@ -9,40 +9,50 @@ use buffer::*;
 //
 // is something like this
 //
-//      h               *
-//     / \             / \
-//    v   b           *   1
-//   / \             / \
-//  b   h           2   *
-//     / \             / \
-//    b   b           3   4
+//      h
+//     / \
+//    v   b
+//   / \
+//  b   h
+//     / \
+//    b   b
 
-#[derive(Debug)]
+// use binary_tree_from_somewhere;
+// type Layout = BinaryTree<LayoutNode>;
+
 pub enum LayoutNode {
     Empty,
-    Buf(i64),
-    Split(Box<Split>),
+    HorizontalSplit,
+    VerticalSplit,
+    Buffer(Buffer)
 }
 
-#[derive(Debug)]
-pub enum Split {
-    Vertical(LayoutNode, LayoutNode),
-    Horizontal(LayoutNode, LayoutNode)
-}
-
-impl LayoutNode {
-    pub fn vertical_split(self) -> LayoutNode {
-        let last = self.last_id();
-        LayoutNode::Split(Box::new(Split::Vertical(self, LayoutNode::Buf(last))))
-    }
-
-    pub fn horizontal_split(self) -> LayoutNode {
-        let last = self.last_id();
-        LayoutNode::Split(Box::new(Split::Horizontal(self, LayoutNode::Buf(last))))
-    }
-
-    pub fn last_id(&self) -> i64 {
-        3
-    }
-}
+//#[derive(Debug)]
+//pub enum LayoutNode {
+//    Empty,
+//    Buf(i64),
+//    Split(Box<Split>),
+//}
+//
+//#[derive(Debug)]
+//pub enum Split {
+//    Vertical(LayoutNode, LayoutNode),
+//    Horizontal(LayoutNode, LayoutNode)
+//}
+//
+//impl LayoutNode {
+//    pub fn vertical_split(self) -> LayoutNode {
+//        let last = self.last_id();
+//        LayoutNode::Split(Box::new(Split::Vertical(self, LayoutNode::Buf(last))))
+//    }
+//
+//    pub fn horizontal_split(self) -> LayoutNode {
+//        let last = self.last_id();
+//        LayoutNode::Split(Box::new(Split::Horizontal(self, LayoutNode::Buf(last))))
+//    }
+//
+//    pub fn last_id(&self) -> i64 {
+//        3
+//    }
+//}
 
